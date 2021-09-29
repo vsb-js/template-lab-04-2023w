160f67010a8475bf5a5e20d468cfd3794bed4a51
Vojtěch Červený <cervik49@gmail.com>
2021-09-29 16:07:35 +0200

    add port

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 
 import express from 'express'
 import fs from 'fs/promises'
+
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
